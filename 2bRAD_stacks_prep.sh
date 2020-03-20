efb9c3848639705148417c0ea15c73ad02e1f7fa
Nathan Whelan <NathanWhelan@users.noreply.github.com>
2020-03-20 13:16:30 -0500

    Updates to Pipeline
    Changed to reflect what was used for L. compacta population genomics study. Also no longer trimming past the 36 base pairs sequenced that are the 2bRAD restriction site

--- 2bRAD_stacks_prep.sh
+++ 2bRAD_stacks_prep.sh
@@ -5,8 +5,6 @@
 ###This script requires BioPerl.
 ###This script requires GNU Parallel
 
-####The AlfIExtract_mod.pl is a modified version by N. Whelan to recognize that first and base pairs were trimmed
-
 #First, I suggest that you move all zipped files into a directory called /raw or other appropriate designation.
 #This pipeline will unzip all files, truncate the files down to the AlfI contig size (minus a bp on either side where N's are more abundant).
 #It will then discard any poor quality sequences and finally pull out only sequences containing the AlfI restriction site.
@@ -14,7 +12,7 @@
 
 ############################
 ##USES 10 cores by Default##
-CORES=8  ##Can be changed##
+CORES=10  ##Can be changed##
 ############################
 
 mkdir raw_backup
@@ -25,17 +23,14 @@ for FILENAME in *.gz
         gunzip $FILENAME
 done
 
-
-ls *.fastq | parallel -j $CORES 'TruncateFastq.pl -i {} -s 2 -e 36 -o {}.out' ##Elim Meyer Scripts  ##Modified to not trim last base as last base of Alf1 site as it is not the last sequenced base pair with 75b
-rm -rf *.fastq
-rename .fastq.out .fastq *.fastq.out
-ls *.fastq |parallel -j $CORES 'perl /usr/local/genome/bin/QualFilterFastq.pl -i {} -m 20 -x 5 -o {}.qual' ##Fix directory if necessary
-rm -rf *.fastq
-rename .fastq.qual .fastq *.fastq.qual
+ls *.fq |parallel -j $CORES 'perl /usr/local/genome/bin/QualFilterFastq.pl -i {} -m 20 -x 5 -o {}.qual' ##Fix directory if necessary
+rm -rf *.fq
+rename .fq.qual .fastq *.fq.qual
 ls *.fastq | parallel -j $CORES 'fastq_to_fasta {} > {}.fasta' #SHRiMP_2_2_3
 rm -rf *.fastq
 rename .fastq.fasta .fasta *.fastq.fasta
 
 
-ls *.fasta | parallel -j $CORES 'AlfIExtract_mod.pl {} {}.alf.fasta'
-rename fasta.alf.fasta .fasta *fasta.alf.fasta
+ls *.fasta | parallel -j $CORES 'AlfIExtract.pl {} {}.alf.fasta'
+rename .fasta.alf.fasta .fasta *.fasta.alf.fasta
+
